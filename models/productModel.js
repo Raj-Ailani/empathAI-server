84484e10353ecde11062a1a6aaf29ff0f5885beb
Raj-Ailani <ailaniraj1@gmail.com>
2021-08-15 17:20:57 +0000

    Added new fields in products

--- models/productModel.js
+++ models/productModel.js
@@ -8,6 +8,14 @@ const productSchema = mongoose.Schema({
     description:{
         type:String,
         required:true,
+    },
+    image:{
+      type:String,
+      required:true,
+    },
+    price:{
+      type:Number,
+      required:true,
     }
 },{
     timestamps: true
